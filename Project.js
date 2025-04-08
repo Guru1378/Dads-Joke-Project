3170c4a061a86fc1460c5976a98107efd97a54ba
GURU PRASAD NOOLIVEEDU <guruprasadndvsvce2021csm@gmail.com>
2025-04-08 09:36:41 +0530

    Hello Guru

---
+++ Project.js
@@ -0,0 +1,48 @@
+let btnid=document.getElementById("btn")
+let api_id=document.getElementById("api_data")
+
+const apiUrl = "https://icanhazdadjoke.com/"
+
+
+let count=0
+function fetchdata(){
+    if (count>=10){
+        api_id.innerText="🔥 You heated my mind, let me drink some cold water! 💧😂"
+        btnid.disabled=true
+        btnid.style.backgroundColor="grey"
+        setTimeout(()=>{
+            btnid.disabled=false
+            btnid.style.backgroundColor = "#ff6b81";
+            api_id.innerText="i am refreshed heat up my brain again"
+            count=0
+        },10000)
+    
+
+    }
+    else{fetch(apiUrl,{
+       headers:{
+        Accept:"application/json"
+       }
+    }).then((res)=>{
+        console.log(res)
+        return res.json()
+    }).then((data)=>{
+        
+        console.log(data)
+        api_id.innerText=`${data.joke}`
+        count++
+        
+        
+        
+    }).catch((err)=>{
+        console.log(err)
+    })
+    
+    
+    }
+}
+
+fetchdata()
+
+btnid.addEventListener("click",fetchdata)
+
